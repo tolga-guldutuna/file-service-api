790ef5daca3f597f76732e943e347baa424036d5
TOLGA GULDUTUNA <tolga.guldutuna@globalbilgi.com.tr>
2025-11-23 12:35:12 +0300

    third commit : neccesary intro file ======================= add: create main application class for File Service API

---
+++ src/main/java/com/fileservice/FileServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FileServiceApiApplication.class, args);
+  }
+
+}
